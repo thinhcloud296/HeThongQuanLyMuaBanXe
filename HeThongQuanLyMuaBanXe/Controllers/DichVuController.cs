5841e161b4cd10ea10b7eb8bfc2259582966caa3
Tran Tan Tai <taitran.february@com>
2024-11-29 20:40:52 +0700

    full code

--- HeThongQuanLyMuaBanXe/Controllers/DichVuController.cs
+++ HeThongQuanLyMuaBanXe/Controllers/DichVuController.cs
@@ -10,29 +10,46 @@ namespace HeThongQuanLyMuaBanXe.Controllers
     public class DichVuController : Controller
     {
         // GET: DichVu
-        public ActionResult Index(int trang = 1)
+        public ActionResult Index(string search = "", int trang = 1)
         {
             CompanyDbContext db = new CompanyDbContext();
-            List<ThongTinDichVu> allDichVus = db.ThongTinDichVus.ToList();
 
-           
-            return View(allDichVus);
+            var dichVus = db.ThongTinDichVus.AsQueryable();
+            if (!string.IsNullOrEmpty(search))
+            {
+                dichVus = dichVus.Where(d => d.TenDichVu.Contains(search));
+            }
+            int pageSize = 6;
+            int pageNumber = trang;
+            int totalItems = dichVus.Count();
+            int totalPages = (int)Math.Ceiling((double)totalItems / pageSize);
+
+            var pagedDichVus = dichVus.OrderBy(d => d.TenDichVu)
+                                       .Skip((pageNumber - 1) * pageSize)
+                                       .Take(pageSize)
+                                       .ToList();
+
+            ViewBag.SearchTerm = search;
+            ViewBag.TotalPages = totalPages;
+            ViewBag.CurrentPage = trang;
+
+            return View(pagedDichVus);
         }
 
-        public ActionResult ChiTietDichVu(string id) 
+        public ActionResult ChiTietDichVu(string id)
         {
             if (id == null)
             {
-                return HttpNotFound(); 
+                return HttpNotFound();
             }
 
             CompanyDbContext db = new CompanyDbContext();
 
-            var dichVu = db.ThongTinDichVus.FirstOrDefault(d => d.MaDichVu == id); 
+            var dichVu = db.ThongTinDichVus.FirstOrDefault(d => d.MaDichVu == id);
 
             if (dichVu == null)
             {
-                return HttpNotFound(); 
+                return HttpNotFound();
             }
 
             return View(dichVu);
--- HeThongQuanLyMuaBanXe/Controllers/NhaCungCapController.cs
+++ HeThongQuanLyMuaBanXe/Controllers/NhaCungCapController.cs
@@ -10,35 +10,40 @@ namespace HeThongQuanLyMuaBanXe.Controllers
     public class NhaCungCapController : Controller
     {
         // GET: NhaCungCap
-        
-        public ActionResult Index()
+        public ActionResult Index(string search = "", int trang = 1)
         {
             CompanyDbContext db = new CompanyDbContext();
-            List<NhaCungCap> allNhaCungCaps = db.NhaCungCaps.ToList();
+            var allNhaCungCaps = db.NhaCungCaps.Where(n => n.TenNhaCungCap.Contains(search)).ToList();
 
+            int NoOfRecordPerPage = 6; 
+            int NoOfPages = Convert.ToInt32(Math.Ceiling(Convert.ToDouble(allNhaCungCaps.Count) / NoOfRecordPerPage));
+            int NoOfRecordToSkip = (trang - 1) * NoOfRecordPerPage;
+
+            ViewBag.Trang = trang;
+            ViewBag.NoOfPages = NoOfPages;
+
+            allNhaCungCaps = allNhaCungCaps.Skip(NoOfRecordToSkip).Take(NoOfRecordPerPage).ToList();
 
             return View(allNhaCungCaps);
         }
 
-        
         public ActionResult ChiTietNhaCungCap(string id)
         {
             if (id == null)
             {
-                return HttpNotFound();
+                return HttpNotFound(); 
             }
 
             CompanyDbContext db = new CompanyDbContext();
 
-            var nhaCungCap = db.NhaCungCaps.FirstOrDefault(d => d.MaNhaCungCap == id);
+            var nhaCungCap = db.NhaCungCaps.FirstOrDefault(d => d.MaNhaCungCap == id); 
 
             if (nhaCungCap == null)
             {
                 return HttpNotFound();
             }
 
-            return View(nhaCungCap);
+            return View(nhaCungCap); 
         }
-
     }
 }
