54fb44bd5d7eb19c2a4fd7e794241badf4743131
Tran Tan Tai <taitran.february@com>
2024-11-29 16:59:06 +0700

    update

---
+++ HeThongQuanLyMuaBanXe/Controllers/DichVuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HeThongQuanLyMuaBanXe.Controllers
+{
+    public class DichVuController : Controller
+    {
+        // GET: DichVu
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
